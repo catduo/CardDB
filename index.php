022009a20f3f248d6b89ce0fde46b1de49e31275
CatDuo <dg@catduo.com>
2013-12-25 03:57:36 +0000

    crossdomain policy file

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
 
     if (isset($_GET['reset'])) $db->resetDatabase(); //TODO make this more production oriented
     if (!isset($_POST['type'])){
-    $question = $db->getQuestion($_POST['type']);
-    echo "<question>";
-    echo $question[Question];
-    echo "</question>";
-    echo "<answer>";
-    echo $question[Answer];
-    echo "</answer>";
+        $question = $db->getQuestion($_POST['type']);
+        echo "<question>";
+        echo $question['Question'];
+        echo "</question>";
+        echo "<answer>";
+        echo $question['Answer'];
+        echo "</answer>";
+    }
 ?>
