5f5e3e44f2e30a96192304591a35acef8eda1429
David Geisert <catduogames@gmail.com>
2013-12-24 19:13:19 -0800

    initial setup

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    require_once("connect.php");
+    
+    $db = new DB();
+    $db->connect($ip,$username,$password,"test");
+
+    if (isset($_GET['reset'])) $db->resetDatabase(); //TODO make this more production oriented
+    
+    $users = $db->getUsers();
+    foreach ($users as $user) {
+        unset($user['passwordhash']);
+        echo "<pre>";
+        print_r($user);
+        echo "</pre>";
+        //echo "<div><b>$user[email]</b>: $user[created]</div>";
+    }
+echo $ip;
+?>
