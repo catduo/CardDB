6e5b664b4a0fe66edd9ed0936251839f6a2cb5ad
David Geisert <catduogames@gmail.com>
2013-12-30 13:20:27 -0800

    changing type to category and type is the style of question

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     $db->connect($ip,$username,$password,$dbname);
 
     if (isset($_GET['reset'])) $db->resetDatabase(); //TODO make this more production oriented
-    if (isset($_POST['type'])){
-        $question = $db->getQuestion('Geography');
+    if (isset($_POST['category'])){
+        $question = $db->getQuestion($_POST['category']);
         echo "<question>";
         echo $question['Question'];
         echo "</question>";
+        echo "<type>"
+        echo $question['Type'];
+        echo "</type>";
         echo "<answer>";
         echo $question['Answer'];
         echo "</answer>";
