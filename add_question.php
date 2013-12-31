afbe4038392b0d7fb6d4c84718d7c3fa08a9169e
David Geisert <catduogames@gmail.com>
2013-12-30 20:16:35 -0800

    forcing the addquestion.php

--- add_question.php
+++ add_question.php
@@ -2,12 +2,7 @@
     require_once("connect.php");
     
     $db = new DB();
-    if (isset($_POST['category'])){
-        $db->addQuestion($_POST['question'],$_POST['answer'],$_POST['type'],$_POST['category']);
-	echo "question added";
-        return;
-    }
-    else{
-        echo "incomplete information";
-    }
+    $db->addQuestion($_POST['question'],$_POST['answer'],$_POST['type'],$_POST['category']);
+    echo "question added";
+    return;
 ?>
