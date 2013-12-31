b7e4e5942234fcfda3c2a057cde99441d0f908ee
David Geisert <catduogames@gmail.com>
2013-12-30 20:26:15 -0800

    Revert "changing addquestion.php"
    This reverts commit 3883239627bdf7bf2c188ab3b3cb6ab7bf14f394.

--- add_question.php
+++ add_question.php
@@ -2,12 +2,11 @@
     require_once("connect.php");
     
     $db = new DB();
+    $db->connect($ip,$username,$password,$dbname);
+
+    if (isset($_GET['reset'])) $db->resetDatabase(); //TODO make this more production oriented
     if (isset($_POST['category'])){
         $db->addQuestion($_POST['question'],$_POST['answer'],$_POST['type'],$_POST['category']);
-	echo "question added";
         return;
-    }
-    else{
-        echo "incomplete information";
     }
 ?>
